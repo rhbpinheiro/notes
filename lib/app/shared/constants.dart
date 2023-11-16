def062c027d37b719a1c9017dcb43ff31179887c
Rodolpho Pinheiro <rhbpinheiro30@gmail.com>
2023-11-16 20:49:01 -0300

    Alterações de imagens

--- lib/app/shared/constants.dart
+++ lib/app/shared/constants.dart
@@ -4,7 +4,9 @@ const apiUrl = 'https://65463306fe036a2fa955548c.mockapi.io/api';
 
 const LinearGradient gradient = LinearGradient(
   colors: [
-    Color(0xFF1f5466),
+    Color(
+      0xFF1f5466,
+    ),
     Color(
       0xFF2c8f8a,
     ),
